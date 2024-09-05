2d9eec30d818cae330567280b956022c072d553e
San <sganis@gmail.com>
2024-09-05 09:03:30 +0300

    v1.0.19

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,6 @@ use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
     Method,
 };
-// use axum::{
-//     http::HeaderValue,
-//     response::{Html, IntoResponse},
-//     routing::get,
-//     Json, Router,
-// };
-// use dotenvy::dotenv;
-// use std::net::SocketAddr;
 
 use route::create_router;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
@@ -60,53 +52,16 @@ pub async fn axum(
             "http://localhost:8000".parse().unwrap(),
             "http://localhost:5173".parse().unwrap(),
         ])
-        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_methods([Method::GET, Method::POST])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     let router = create_router(Arc::new(AppState { db: pool.clone() }))
         .layer(cors)
+        // to explore the metadata resource
         .route("/metadata", axum::routing::get(format!("{:?}", metadata)));
 
     println!("Server started successfully");
 
     Ok(router.into())
 }
-
-// #[tokio::main]
-// async fn main() {
-//     dotenv().expect(".env not found");
-//     let database_url = std::env::var("DATABASE_URL").unwrap();
-//     let pool = match PgPoolOptions::new()
-//         .max_connections(10)
-//         .connect(&database_url)
-//         .await
-//     {
-//         Ok(pool) => {
-//             println!("Connection to the database is successful!");
-//             pool
-//         }
-//         Err(err) => {
-//             println!("Failed to connect to the database: {:?}", err);
-//             std::process::exit(1);
-//         }
-//     };
-
-//     let cors = CorsLayer::new()
-//         .allow_origin([
-//             "http://localhost:8000".parse().unwrap(),
-//             "http://localhost:5173".parse().unwrap(),
-//         ])
-//         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
-//         .allow_credentials(true)
-//         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
-
-//     let router = create_router(Arc::new(AppState { db: pool.clone() })).layer(cors);
-//     println!("Server started successfully");
-
-//     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-//     axum::Server::bind(&addr)
-//         .serve(router.into_make_service())
-//         .await
-//         .unwrap();
-// }
